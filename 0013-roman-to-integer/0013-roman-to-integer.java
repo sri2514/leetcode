03c30364d63c4af7754f9cd67a3070728990ca17
SRINITHI D <146941644+sri2514@users.noreply.github.com>
2025-08-21 14:51:17 +0530

    Time: 6 ms (20.94%), Space: 45.3 MB (9.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,20 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character , Integer> hm = new HashMap<>();
-        hm.put('I',1);
-        hm.put('V',5);
-        hm.put('X',10);
-        hm.put('L',50);
-        hm.put('C',100);
-        hm.put('D',500);
-        hm.put('M',1000);
-        int result = 0;
+        //String[]  symbols = {"I","V","X","L","C","D","M"}
+        //int[] val = {1,5,10,50,100,500,1000}
+        Map<Character,Integer> mp = new HashMap<>();
+        mp.put('I',1);
+        mp.put('V',5);
+        mp.put('X',10);
+        mp.put('L',50);
+        mp.put('C',100);
+        mp.put('D',500);
+        mp.put('M',1000);
+        int results = 0;
         for(int i = 0 ; i < s.length() - 1 ; i++){
-            if(hm.get(s.charAt(i)) < hm.get(s.charAt(i + 1)))
-            result -= hm.get(s.charAt(i));
+            if(mp.get(s.charAt(i)) < mp.get(s.charAt(i + 1)))
+            results -= mp.get(s.charAt(i));
             else
-            result += hm.get(s.charAt(i));
+            results += mp.get(s.charAt(i));
         }
-        return result + hm.get(s.charAt(s.length() - 1));
+        return results + mp.get(s.charAt(s.length() - 1));
     }
 }
