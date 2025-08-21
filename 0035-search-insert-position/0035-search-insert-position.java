1cf93ca5119bb78c6a0385290f59bf2cf9ebbcbc
SRINITHI D <146941644+sri2514@users.noreply.github.com>
2025-08-21 12:16:14 +0530

    Time: 117 ms (0.72%), Space: 43.2 MB (28.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,8 +4,8 @@ class Solution {
         if(nums[nums.length - 1] < target) return nums.length;
         for(int i = 0 ; i < nums.length - 1 ; i++){
             if(nums[i] == target) return i;
-            if(nums[i + 1] == target) return i + 1;
-            if(nums[i] < target && nums[i + 1] > target )
+            else if(nums[i + 1] == target) return i + 1;
+            else if(nums[i] < target && nums[i + 1] > target )
             return i + 1;
         }
         return 0;
